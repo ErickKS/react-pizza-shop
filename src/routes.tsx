e607440012f1fd0355a61d0ceba0cf974d1343f7
Erick <erick.kuwahara79@gmail.com>
2024-03-15 16:24:41 -0300

    feat: dashboard & 404 page

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,13 +5,15 @@ import { AuthLayout } from "./pages/_layouts/auth";
 
 import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
-import { Dashboard } from "./pages/app/dashboard";
+import { Dashboard } from "./pages/app/dashboard/dashboard";
 import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/404";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: "/", element: <Dashboard /> },
       { path: "/orders", element: <Orders /> },
